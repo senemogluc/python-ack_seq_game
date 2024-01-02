051d5869ed2e72a35607e5eefbbd17621ad492df
senemogluc <cagin.senemoglu@gmail.com>
2024-01-02 22:41:57 +0300

    implemented client

--- client.py
+++ client.py
@@ -1,48 +1,38 @@
 import socket
 
-# Set up the socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# Set up the initial sequence and acknowledgement numbers
-seq = 0
-ack = 0
-
-while True:
-    # Get the input from the user as a string
-    input_string = input("Enter the ACK, SEQ, and length values: ")
-
-    # Split the input string on the comma delimiter to get the individual values
-    seq, ack, length = input_string.split(',')
-
-    # Convert the values to the desired data type
-    seq = int(seq)
-    ack = int(ack)
-    length = int(length)
-
-    # Get the packet length from the user
-    #length = int(input("Enter the packet length: "))
-
-    # Calculate the next sequence and acknowledgement numbers for the client's message
-    #seq += length
-    #ack += length
-
-    # Send a message with the client's sequence and acknowledgement numbers and packet length
-    message = f"{seq},{ack},{length}"
-
-    # Convert the message to a bytes-like object before sending it
+def send_message(sock, seq, ack, length=10, addr = ('localhost', 12345)):
+    message = f"{seq} {ack} {length}"
     message = message.encode()
-    sock.sendto(message, ('localhost', 12345))
-    
-    # Receive a message from the server
-    data, _ = sock.recvfrom(1024)
+    sock.sendto(message, addr)
 
-    # Convert the received bytes-like object to a string
+def receive_message(sock):
+    data, addr = sock.recvfrom(1024)
     data = data.decode()
+    print(f"Received: {data} from {addr}")
+    received_seq, received_ack, received_len = data.split(' ')
+    received_seq = int(received_seq)
+    received_ack = int(received_ack)
+    received_len = int(received_len)
 
-    # Extract the server's sequence and acknowledgement numbers and packet length from the message
-    server_seq, server_ack, _ = data.split(',')
-    server_seq = int(server_seq)
-    server_ack = int(server_ack)
+    return received_seq, received_ack, received_len, addr
 
-    # Print the server's response
-    print(f"Server response: seq={server_seq}, ack={server_ack}")
+def check_lost(received_seq, ack):
+        return received_seq != ack  
+
+def main():   
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    iteration = 0
+    while True:
+        input_string = input("Enter the SEQ, ACK, and length values: ")
+        seq, ack, length = list(map(int, input_string.split(' ')))
+
+        send_message(sock, seq, ack, length)
+        received_seq, received_ack, received_len, addr = receive_message(sock)
+        if check_lost(received_seq, ack):
+            print("Packet lost!")
+        iteration += 1
+        print("Iteration: ", iteration)
+
+if __name__ == "__main__":
+    main()
